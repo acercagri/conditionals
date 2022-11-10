a47887b35331e3cd2430ea307b5708f4976ae8ca
acercagri <cagri-acer@outlook.com>
2022-11-10 11:49:13 +0300

    conditionals

---
+++ src/conditionals/conditionals.java
@@ -0,0 +1,18 @@
+package conditionals;
+
+public class conditionals {
+
+	public static void main(String[] args) {
+		int vade = 20;
+		if(vade>20) {
+			System.out.println("1548 TL");
+		}
+		else if(20>vade) {
+			System.out.println("1148 TL");
+		}
+		else {
+			System.out.println(1348);
+		}
+	}
+
+}
